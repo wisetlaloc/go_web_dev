b771e86049061c5976faf68badd284148f5924cf
wisetlaloc <13391342+wisetlaloc@users.noreply.github.com>
2019-02-28 08:04:42 +0900

    add tcp samples

---
+++ hands-on_set4/07_hands-on/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer li.Close()
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go serve(conn)
+	}
+}
+
+func serve(conn net.Conn) {
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if ln == "" {
+			fmt.Println("This is the end of the http request")
+			break
+		}
+	}
+
+	//fmt.Println("Code got here.")
+	io.WriteString(conn, "I see you connected.")
+
+	conn.Close()
+
+}
